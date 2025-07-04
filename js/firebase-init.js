0aafa2f69550048cdb0a2aa962ba1f4e4574aad4
GEFigueroaW <eugenfw@gmail.com>
2025-07-04 17:19:27 -0600

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,14 @@
+// /js/firebase-init.js
+
+// Configuración del proyecto Firebase (REEMPLAZA estos datos)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
