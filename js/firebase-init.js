2a1c0a12c4d9523ef8cf5575a1a451b42166ef2e
GEFigueroaW <eugenfw@gmail.com>
2025-07-05 10:31:53 -0600

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,17 +1,15 @@
 // /js/firebase-init.js
 
-// Configuración del proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB4bCGyyPuQo-3-ONMPFKtqPEJDFl8Cb54",
   authDomain: "ya-me-vi.firebaseapp.com",
   projectId: "ya-me-vi",
-  storageBucket: "ya-me-vi.appspot.com", // 🔧 Corregido
+  storageBucket: "ya-me-vi.appspot.com",
   messagingSenderId: "748876890843",
   appId: "1:748876890843:web:070d1eb476d38594d002fe",
   measurementId: "G-D7R797S5BC"
 };
 
-// Inicializar Firebase solo si no está inicializado ya
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
