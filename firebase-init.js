4b574f8d2d34f6baffb5b339a97c0dc5248fe5ac
GEFigueroaW <eugenfw@gmail.com>
2025-07-07 20:49:43 -0600

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCScJA-UGs3WcBnfAm-6K94ybZ4bzBahz8",
+  authDomain: "brain-storm-8f0d8.firebaseapp.com",
+  projectId: "brain-storm-8f0d8",
+  storageBucket: "brain-storm-8f0d8.appspot.com",
+  messagingSenderId: "401208607043",
+  appId: "1:401208607043:web:6f35fc81fdce7b3fbeaff6"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
