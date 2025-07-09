8f463bf399c5af4528a300b6f19e2bc9032b2f97
GEFigueroaW <eugenfw@gmail.com>
2025-07-09 16:17:10 -0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,24 @@
 // firebase-init.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB4bCGyyPuQo-3-ONMPFKtqPEJDFl8Cb54",
   authDomain: "ya-me-vi.firebaseapp.com",
   projectId: "ya-me-vi",
-  storageBucket: "ya-me-vi.firebasestorage.app",
+  storageBucket: "ya-me-vi.appspot.com",
   messagingSenderId: "748876890843",
   appId: "1:748876890843:web:070d1eb476d38594d002fe",
   measurementId: "G-D7R797S5BC"
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
+export { auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, db, doc, getDoc, setDoc };
