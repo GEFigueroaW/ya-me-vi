69dd1aac197f5c727dc1fe399e4ceda61379c3be
GEFigueroaW <eugenfw@gmail.com>
2025-07-11 10:46:29 -0600

    Delete js/auth-protect.js

--- js/auth-protect.js
+++
@@ -1,8 +0,0 @@
-// js/auth-protect.js
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (!user) {
-    // Si no hay usuario logueado, redirige al inicio de sesión
-    window.location.href = 'index.html';
-  }
-});
