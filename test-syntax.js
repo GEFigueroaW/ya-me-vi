c6bfa192861736d436ec7e88eb3fbfb5ccd7dd5e
GEFigueroaW <eugenfw@gmail.com>
2025-07-14 15:40:28 -0600

    Delete test-syntax.js

--- test-syntax.js
+++
@@ -1,8 +0,0 @@
-// Test file to check syntax
-function mostrarEstadisticasComparativas(datosPorSorteo) {
-  console.log('Test function');
-  return true;
-}
-
-// Test call
-mostrarEstadisticasComparativas({});
